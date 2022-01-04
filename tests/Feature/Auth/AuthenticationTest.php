e957752eb4dfb3390ac2473983d9f3f566be007d
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2022-01-04 17:09:25 +0800

    Authentication test case

---
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_users_can_login()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->postJson('/api/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJsonStructure(['access_token']);
+    }
+
+    public function test_users_can_not_login_with_invalid_credentials()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->postJson('/api/login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+
+        $response->assertStatus(401)
+            ->assertJson(['message' => 'Invalid credentials']);
+    }
+}
---
+++ tests/Feature/Auth/RegistrationTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function test_new_users_can_register()
+    {
+        $response = $this->postJson('/api/register', [
+            'email' => $this->faker()->email,
+            'password' => $this->faker()->password,
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJson(['message' => 'User successfully registered']);
+    }
+
+    public function test_new_users_can_not_register_with_existing_email()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->postJson('/api/register', [
+            'email' => $user->email,
+            'password' => $this->faker()->password,
+        ]);
+
+        $response->assertStatus(400)
+            ->assertJson(['message' => 'Email already taken']);
+    }
+}
