a8e407881b0d3ce245b5e5d451b10d5db15af37f
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2022-01-04 17:09:15 +0800

    Authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        return response()->json(['access_token' => $token], 201);
+    }
+
+    /**
+     * Register new user.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:3'
+        ]);
+
+        if (User::where('email', $request->email)->exists()) {
+            return response()->json(['message' => 'Email already taken'], 400);
+        }
+
+        User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json(['message' => 'User successfully registered'], 201);
+    }
+}
